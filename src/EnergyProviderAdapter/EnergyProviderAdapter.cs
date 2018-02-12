6da78feb928a6dcefee934a6be0501e532b519ee
ayarshou <ruff.manbearpig@gmail.com>
2018-02-12 13:36:25 +0300

    EN-2086 Added skeleton IProviderAdapter Project

---
+++ src/EnergyProviderAdapter/EnergyProviderAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CTM.Quoting.Provider;
+
+namespace EnergyProviderAdapter
+{
+    public class EnergyProviderAdapter : IProviderAdapter<EnergyEnquiry, EnergyQuote>
+    {
+        private readonly string providerName;
+        private readonly string brandCodePrefix;
+
+        public EnergyProviderAdapter(string providerName, string brandCodePrefix)
+        {
+            this.providerName = providerName;
+            this.brandCodePrefix = brandCodePrefix;
+        }
+
+        public Task<QuoteResult<EnergyQuote>> GetQuotes(MakeProviderEnquiry<EnergyEnquiry> providerEnquiry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/EnergyProviderAdapter/EnergyProviderAdapterModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using CTM.Quoting.Provider;
+
+namespace EnergyProviderAdapter
+{
+    public class EnergyProviderAdapterModule: Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            RegisterProviderAdapter(builder, Provider.Name, Provider.BrandCodePrefix);
+        }
+
+        private static void RegisterProviderAdapter(ContainerBuilder builder, string providerName, string brandCodePrefix)
+        {
+            builder.RegisterType<EnergyProviderAdapter>()
+                .AsImplementedInterfaces()
+                .WithMetadata<ProviderAdapterMetadata>(
+                    m => m
+                        .For(am => am.ProviderName, providerName)
+                        .For(am => am.SupportedProduct, Product.Parse("Energy/1.0")))
+                .WithMetadata("messaging:component.queueName", $"quoting:Energy:{providerName}:1.0")
+                .WithParameter("providerName", providerName)
+                .WithParameter("brandCodePrefix", brandCodePrefix);
+        }
+    }
+}
