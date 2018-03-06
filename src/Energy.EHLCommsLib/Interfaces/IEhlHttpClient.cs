c9c98ef70c86e74e60d5b0d5ba6b6a578b10b747
ayarshou <ruff.manbearpig@gmail.com>
2018-03-06 11:59:29 +0300

    EN-2187 Added interface for EHL

---
+++ src/Energy.EHLCommsLib/Interfaces/IEhlHttpClient.cs
@@ -0,0 +1,15 @@
+﻿using Energy.EHLCommsLib.Contracts.Responses;
+using Energy.EHLCommsLib.Models;
+
+namespace Energy.EHLCommsLib.Interfaces
+{
+    public interface IEhlHttpClient
+    {
+        SwitchesApiResponse GetApiDataTemplate(string url, string rel);
+
+        //T GetSwitchesApiPostResponse<T>(string url, T responseDataToSend, string relKey, BaseRequest request)
+        //   where T : ApiResponse, new();
+
+        //T GetSwitchesApiGetResponse<T>(string url, string relKey, BaseRequest request) where T : ApiResponse, new();
+    }
+}
