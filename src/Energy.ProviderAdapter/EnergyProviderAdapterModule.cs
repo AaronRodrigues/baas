56241236e95d173ae8bfd96aa619b49eafe885fd
Muhammad Waqas Raza <muhammad.waqasraza@bglgroup.co.uk>
2018-03-07 10:46:16 +0000

    EN-2097 <MRaza> Adding simple fake build scipt

--- src/Energy.ProviderAdapter/EnergyProviderAdapterModule.cs
+++ src/Energy.ProviderAdapter/EnergyProviderAdapterModule.cs
@@ -17,8 +17,8 @@ namespace Energy.ProviderAdapter
                 .WithMetadata<ProviderAdapterMetadata>(
                     m => m
                         .For(am => am.ProviderName, providerName)
-                        .For(am => am.SupportedProduct, Product.Parse("Energy/1.0")))
-                .WithMetadata("messaging:component.queueName", string.Format("quoting:Energy:{0}:1.0", providerName))
+                        .For(am => am.SupportedProduct, Product.Parse("Energy/2.0")))
+                .WithMetadata("messaging:component.queueName", string.Format("quoting:Energy:{0}:2.0", providerName))
                 .WithParameter("providerName", providerName)
                 .WithParameter("brandCodePrefix", brandCodePrefix);
         }
