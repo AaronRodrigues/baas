b9c6795aa591bd226a944d2bb6993467e3e35e40
Viktor K <Viktar.Kachan@bglgroup.co.uk>
2018-03-20 18:15:05 +0300

    EN-2185 <Viktor K, Richard C> Checking existing and proposing a new schema for adaptor request.

--- src/Energy.EHLCommsLib/Models/Prices/GetPricesRequest.cs
+++ src/Energy.EHLCommsLib/Models/Prices/GetPricesRequest.cs
@@ -3,43 +3,61 @@ using Energy.EHLCommsLib.Enums;
 
 namespace Energy.EHLCommsLib.Models.Prices
 {
+
     public class GetPricesRequest : BaseRequest
     {
-        public string CurrentSupplyUrl { get; set; }
-        public string SwitchUrl { get; set; }
-        public string SuppliersKey { get; set; }
+        //todo refactor: based on calculus
+        /*
+                private bool UseDetailedEstimatorForElectricity
+                {
+                    get
+                    {
+                        if (ComparingGasOnly)
+                            return false;
+
+                        return NoBillViewModel.ElectricitySpendDontKnow;
+                    }
+                }
+
+                private bool UseDetailedEstimatorForGas
+                {
+                    get
+                    {
+                        if (ComparingElectricityOnly)
+                            return false;
+
+                        return NoBillViewModel.GasSpendDontKnow;
+                    }
+                }
+        */
         public bool UseDetailedEstimatorForElectricity { get; set; }
         public bool UseDetailedEstimatorForGas { get; set; }
-        public string PrePayment { get; set; }
-        public CompareWhat CompareType { get; set; }
+
         public UsageData UsageData { get; } = new UsageData();
         public SpendData SpendData { get; } = new SpendData();
         public EstimatorData EstimatorData { get; } = new EstimatorData();
-        public int GasSupplierId { get; set; }
-        public string GasSupplierName { get; set; }
-        public int GasTariffId { get; set; }
-        public string GasTariffName { get; set; }
-        public string GasTariffType { get; set; }
-        public int GasPaymentMethodId { get; set; }
-        public string GasPaymentMethodName { get; set; }
-        public int ElectricitySupplierId { get; set; }
-        public string ElectricitySupplierName { get; set; }
-        public string ElectricityTariffType { get; set; }
-        public int ElectricityTariffId { get; set; }
-        public string ElectricityTariffName { get; set; }
-        public int ElectricityPaymentMethodId { get; set; }
-        public string ElectricityPaymentMethodName { get; set; }
-        public bool ElectricityEco7 { get; set; }
-        public decimal PercentageNightUsage { get; set; }
-        public int Eco7DayUsageValue { get; set; }
-        public int Eco7NightUsageValue { get; set; }
-        public int DefaultElectricitySupplierId { get; set; }
 
         public bool CalculateElecBasedOnBillSpend => SpendData.ElectricitySpendAmount > 0;
-
         public bool CalculateGasBasedOnBillSpend => SpendData.GasSpendAmount > 0;
+        public bool IgnoreProRataComparison { get; set; }
+
+        public string CurrentSupplyUrl { get; set; }
+        public string SwitchUrl { get; set; }
+
+        public string PrePayment { get; set; }
+        public CompareWhat CompareType { get; set; }
+
+
+        public int GasSupplierId { get; set; }
+        public int GasTariffId { get; set; }
+        public int GasPaymentMethodId { get; set; }
+        public int ElectricitySupplierId { get; set; }
+        public int ElectricityTariffId { get; set; }
+        public int ElectricityPaymentMethodId { get; set; }
+        public bool ElectricityEco7 { get; set; }
+        public decimal PercentageNightUsage { get; set; }
+
 
         public Guid JourneyId { get; set; }
-        public bool IgnoreProRataComparison { get; set; }
     }
 }
