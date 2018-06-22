ed6df18624c5f44074cfdc4e0aea871062a188fa
AaronRodrigues <aaronrod@gmail.com>
2018-06-22 12:04:50 +0100

    EN-2457 <oli/aaron/rob> Increases iterations

--- src/Energy.ProviderAdapterMemoryProfiling/Program.cs
+++ src/Energy.ProviderAdapterMemoryProfiling/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Energy.ProviderAdapterTests;
 
@@ -10,14 +11,29 @@ namespace Energy.ProviderAdapterMemoryProfiling
 {
     class ScenarioRunner : OutsideInTestBase
     {
+        private static float CalculateMemoryUsedInMb()
+        {
+            var memoryPerformanceCounter = new PerformanceCounter(
+                "Process",
+                "Working Set",
+                Process.GetCurrentProcess().ProcessName);
+
+            var memoryUsedInBytes = memoryPerformanceCounter.NextValue();
+            var memoryUsedInMb = memoryUsedInBytes / 1024.0f / 1024.0f;
+            Console.WriteLine(memoryUsedInMb);
+            return memoryUsedInMb;
+        }
+
         public void Run()
         {
             Given_the_provider_adapter_is_loaded();
             Given_EHL_API_is_working_correctly();
             Given_a_valid_enquiry();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 When_prices_are_requested_for_production_environment().Wait();
+                CalculateMemoryUsedInMb();
+                Thread.Sleep(100);
             }
         }
     }
