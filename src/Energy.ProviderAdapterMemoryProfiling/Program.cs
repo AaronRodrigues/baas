21d6b0fb439603faba4581f83ea5861a3ccb6bc5
AaronRodrigues <aaronrod@gmail.com>
2018-06-22 11:34:09 +0100

    EN-2457 <aaron/rob/oli> Sets up project and logs memory for one instance :sparkles::metal:

---
+++ src/Energy.ProviderAdapterMemoryProfiling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Energy.ProviderAdapterTests;
+
+namespace Energy.ProviderAdapterMemoryProfiling
+{
+    class ScenarioRunner : OutsideInTestBase
+    {
+        public void Run()
+        {
+            Given_the_provider_adapter_is_loaded();
+            Given_EHL_API_is_working_correctly();
+            Given_a_valid_enquiry();
+            for (int i = 0; i < 5; i++)
+            {
+                When_prices_are_requested_for_production_environment().Wait();
+            }
+        }
+    }
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var scenarioRunner = new ScenarioRunner();
+            scenarioRunner.Run();
+            return CalculateMemoryUsedInMb()  < 1000f ? 1 : 0;
+        }
+        private static float CalculateMemoryUsedInMb()
+        {
+            var memoryPerformanceCounter = new PerformanceCounter(
+                "Process",
+                "Working Set",
+                Process.GetCurrentProcess().ProcessName);
+
+            var memoryUsedInBytes = memoryPerformanceCounter.NextValue();
+            var memoryUsedInMb = memoryUsedInBytes / 1024.0f / 1024.0f;
+            Console.WriteLine(memoryUsedInMb);
+            return memoryUsedInMb;
+        }
+    }
+
+
+}
