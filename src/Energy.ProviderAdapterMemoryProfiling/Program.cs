ca64cf41a2b2bbc3ae66183c31b82dbd2ce13056
AaronRodrigues <aaronrod@gmail.com>
2018-06-26 11:02:45 +0100

    EN-2457 <aaron/oli> Changes C# version from 7.1 to default(7.0) :sparkles::sadparrot: -Need to change MS Build version in the future

--- src/Energy.ProviderAdapterMemoryProfiling/Program.cs
+++ src/Energy.ProviderAdapterMemoryProfiling/Program.cs
@@ -4,11 +4,11 @@ namespace Energy.ProviderAdapterMemoryProfiling
 {
     class Program
     {
-        static async Task<int> Main()
+        static int Main()
         {
             var scenarioRunner = new ScenarioRunner();
             scenarioRunner.Initialize();
-            var success = await scenarioRunner.Run();
+            var success = scenarioRunner.Run().Result;
             return success ? 0 : 1;
         }
     }
