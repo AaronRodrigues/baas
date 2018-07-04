6bf4b2aac2edaa24d11d6971f6b869d5689a2e31
alexanderwjrussell <awjrussell@live.co.uk>
2018-07-04 14:15:33 +0100

    Correcting SetUp to use new When_a_Valid_enquiry

--- src/Energy.ProviderAdapterTests/MemoryProfiler.cs
+++ src/Energy.ProviderAdapterTests/MemoryProfiler.cs
@@ -6,7 +6,7 @@
         {
             Given_the_provider_adapter_is_loaded();
             Given_EHL_API_is_working_correctly(false);
-            Given_a_valid_enquiry();
+            When_a_valid_enquiry();
         }
     }
 }
