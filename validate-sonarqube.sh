bf461ab0b7f7091fe9f5be38b2aea7a82869ff36
Oli Wennell <oli.wennell@gmail.com>
2018-07-10 10:48:37 +0100

    EN-2491 Updating validation scripts to point to the right SQ project :eyes:

--- validate-sonarqube.sh
+++ validate-sonarqube.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PROJECTSTATUS=$(curl -b sonarqube.cookies.txt https://sonarqube-security.test.ctmers.io//api/qualitygates/project_status?projectKey=CTM.Allspark.ProviderAdapterTemplate)
+PROJECTSTATUS=$(curl -b sonarqube.cookies.txt https://sonarqube-security.test.ctmers.io//api/qualitygates/project_status?projectKey=EnergyProviderAdapter)
 echo "project status is : " $PROJECTSTATUS
 
-STATUSCODE=$(curl -b sonarqube.cookies.txt https://sonarqube-security.test.ctmers.io//api/qualitygates/project_status?projectKey=CTM.Allspark.ProviderAdapterTemplate | jq '.projectStatus.status')
+STATUSCODE=$(curl -b sonarqube.cookies.txt https://sonarqube-security.test.ctmers.io//api/qualitygates/project_status?projectKey=EnergyProviderAdapter | jq '.projectStatus.status')
 echo "status code is : "$STATUSCODE
 if [ $STATUSCODE != '"OK"' ]; then
     echo "Sonarqube quality criteria not met. Check the dashboard."
