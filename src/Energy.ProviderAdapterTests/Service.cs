8da24525528aaeebc7baaa1e1b6763e06e1bac72
AaronRodrigues <aaronrod@gmail.com>
2018-07-26 14:47:05 +0100

    Tests post :white_check_mark:

--- src/Energy.ProviderAdapterTests/Service.cs
+++ src/Energy.ProviderAdapterTests/Service.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Owin.Testing;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using Owin;
 
+
 namespace Energy.ProviderAdapterTests
 {
+
     class Service
     {
+
         [Test]
         public void Returns_HttpStatusCodeOK_When_GET_Request_Is_Made()
         {
@@ -24,7 +30,23 @@ namespace Energy.ProviderAdapterTests
             Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         }
 
+        [Test]
+        public void Returns_Biscuit_When_A_Biscuit_is_posted()
+        {
+            var testServer = TestServer.Create(app =>
+                app.Run(async context => { context.Response.StatusCode = 200; })
+            );
+            var httpclient = new HttpClient(testServer.Handler);
 
+            var pJsonContent = "biscuit";
+            var httpRequestMessage = new HttpRequestMessage();
+            HttpContent httpContent = new StringContent(pJsonContent, Encoding.UTF8, "application/json");
+            httpRequestMessage.Content = httpContent;
+            var result = httpclient.PostAsync("http://www.biscuitsasaservice.com", httpContent).Result;
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+ 
 
     }
 
@@ -34,13 +56,56 @@ namespace Energy.ProviderAdapterTests
         public string Name;
         public string Id;
     }
-    public class BiscuitProvider
+    public class BiscuitProvider   
     {
-        public void CreateBiscuit(Biscuit biscuit)
+        private readonly HttpClient _client;
+
+        public BiscuitProvider(HttpClient client)
         {
-            // todo: POST to biscuit api with serialised biscuit in the request body
+            _client = client;
+        }
+        public async Task<PostResponse> CreateBiscuit(Biscuit biscuit)
+        {
+            //todo: POST to biscuit api with serialised biscuit in the request body
+ 
+            var jsonRequest = JsonConvert.SerializeObject(biscuit);
+            var newRequest = new HttpRequestMessage(HttpMethod.Post, "http://www.baas.com/recepticle");
+            newRequest.Content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            var response = await _client.SendAsync(newRequest);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.IsSuccessStatusCode ? successfulResponse(response.StatusCode) : unsuccessfulResponse(response.StatusCode);
 
         }
+
+        private PostResponse successfulResponse(HttpStatusCode responseStatusCode)
+        {
+            return new PostResponse
+            {
+                WasSuccessful = true,
+                StatusCode = responseStatusCode
+            };
+        }
+
+        private PostResponse unsuccessfulResponse(HttpStatusCode responseStatusCode)
+        {
+            return new PostResponse
+            {
+                WasSuccessful = false,
+                StatusCode = responseStatusCode
+            };
+        }
+
+        public class PostResponse
+        {
+            public bool WasSuccessful;
+            public HttpStatusCode StatusCode;
+        }
+
+
+
 
         public Biscuit FindBiscuitById(string id)
         {
